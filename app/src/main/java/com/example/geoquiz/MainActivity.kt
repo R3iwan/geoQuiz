f15f2c5de61bc966555fc5faadb6ced8de98c89c
r3iwan <rakhat.study@gmail.com>
2023-10-24 20:26:47 +0600

    Added Previous and Next Buttons with icons

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,35 +1,76 @@
 package com.example.geoquiz
 
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import java.time.ZoneOffset
 
 class MainActivity : ComponentActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_england, true),
+        Question(R.string.question_kazakhstan, true),
+        Question(R.string.question_denmark, false),
+        Question(R.string.question_spain, false),
+        Question(R.string.question_usa, false))
+
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            checkAnswer(true)
         }
         falseButton.setOnClickListener{
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            checkAnswer(false)
         }
 
+        nextButton.setOnClickListener{
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener{
+            currentIndex = (currentIndex - 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+
+        }
+        private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+         }
+
+        private fun checkAnswer(userAnswer: Boolean){
+            val correctAnswer = questionBank[currentIndex].answer
+
+            val messageResId = if (userAnswer == correctAnswer){
+                R.string.correct_toast
+            } else {
+                R.string.incorrect_toast
+            }
+
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
         }
     }
+
 
---
+++ app/src/main/java/com/example/geoquiz/Question.kt
@@ -0,0 +1,4 @@
+package com.example.geoquiz
+
+import androidx.annotation.StringRes
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
