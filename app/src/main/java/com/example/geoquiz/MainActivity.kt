902b90a79372d007ce0d2b11d78389746f539459
r3iwan <rakhat.study@gmail.com>
2023-11-01 21:02:49 +0600

    Changed Cheat feature to Results Button

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,20 +2,11 @@ package com.example.geoquiz
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import android.view.Gravity
-import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import java.time.ZoneOffset
-
-private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     private var currentIndex = 0
@@ -25,16 +16,17 @@ class MainActivity : ComponentActivity() {
         Question(R.string.question_kazakhstan, true),
         Question(R.string.question_denmark, false),
         Question(R.string.question_spain, false),
-        Question(R.string.question_usa, false))
+        Question(R.string.question_usa, false)
+    )
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: ImageButton
     private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
-    private lateinit var cheatButton: Button
+    private lateinit var resultsButton: Button
 
-    private var totalScore = 0
+    var totalScore = 0
     private var sumOfExam = 0
     private var isQuestionAnswered = false
     private var isPrevAnsw = false
@@ -48,66 +40,70 @@ class MainActivity : ComponentActivity() {
         nextButton = findViewById(R.id.next_button)
         prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
-        cheatButton = findViewById(R.id.cheat_button)
+        resultsButton = findViewById(R.id.results_button)
 
         trueButton.setOnClickListener {
-            if(!isQuestionAnswered){
-                checkAnswer(true)
+            if (!isQuestionAnswered) {
+                checkAnswer(true, sumOfExam)
                 isQuestionAnswered = true
                 isPrevAnsw = true
             }
         }
-        falseButton.setOnClickListener{
-            if(!isQuestionAnswered){
-                checkAnswer(false)
+        falseButton.setOnClickListener {
+            if (!isQuestionAnswered) {
+                checkAnswer(false, sumOfExam)
                 isQuestionAnswered = true
                 isPrevAnsw = true
             }
         }
 
-        nextButton.setOnClickListener{
-            if(currentIndex < questionBank.size){
+        nextButton.setOnClickListener {
+            if (currentIndex < questionBank.size) {
                 currentIndex++
-            updateQuestion()
+                updateQuestion()
             }
         }
 
-        prevButton.setOnClickListener{
-            if (currentIndex > 0){
-                if (!isPrevAnsw){
+        prevButton.setOnClickListener {
+            if (currentIndex > 0) {
+                if (!isPrevAnsw) {
                     currentIndex--
                     updateQuestion()
                 }
             }
         }
 
-        cheatButton.setOnClickListener {
-            val intent = Intent(this, CheatActivity::class.java)
-            intent.putExtra("key", "True")
-            startActivity(intent)
-        }
-
         updateQuestion()
 
+        val (value1, value2) = checkAnswer(true, totalScore)
+
+        resultsButton.setOnClickListener {
+            val intent = Intent(this, ResultsActivity::class.java)
+            intent.putExtra("totalScore", value2)
+            this.startActivity(intent)
+
         }
-        private fun updateQuestion() {
+    }
+
+    private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
-        }
+    }
 
-        private fun checkAnswer(userAnswer: Boolean): Boolean {
-            val correctAnswer = questionBank[currentIndex].answer
+    private fun checkAnswer(userAnswer: Boolean, sumOfExam: Int): Pair<Boolean, Int> {
+        val correctAnswer = questionBank[currentIndex].answer
 
-            if (userAnswer == correctAnswer){
-                R.string.correct_toast
-                sumOfExam += 20
-            } else {
-                R.string.incorrect_toast
-            }
-            totalScore = sumOfExam
-            return userAnswer
+        if (userAnswer == correctAnswer) {
+            this.sumOfExam += 20
+            Toast.makeText(this, "True", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "False", Toast.LENGTH_SHORT).show()
         }
+        totalScore = sumOfExam
+        return Pair(userAnswer, this.totalScore)
+    }
 }
+
 
 
 
---
+++ app/src/main/java/com/example/geoquiz/ResultsActivity.kt
@@ -0,0 +1,25 @@
+package com.example.geoquiz
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+
+class ResultsActivity : ComponentActivity(){
+    private var newTotalScore = 0
+    private lateinit var showResultsButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+        showResultsButton = findViewById(R.id.show_results_button)
+
+        val mainActivity = MainActivity()
+        newTotalScore = mainActivity.totalScore
+
+        showResultsButton.setOnClickListener{
+            val toast = Toast.makeText(this, "Your Score is $newTotalScore", Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
